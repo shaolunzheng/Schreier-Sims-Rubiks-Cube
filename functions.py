75c26e297f0e0baed0c0b127471c329c4e409789
shaolunzheng <shaoluncd@gmail.com>
2025-05-12 18:18:22 +0200

    Added an option to check whether an element lies in the subgroup

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-# moves: a list containing multiple moves
+# moves: an np.array containing multiple moves
 # n: order of symmetric group
 # compose: composes the elements in moves in order
 def compose(moves, n):
-    result = np.array(list(range(n)))
+    result = np.arange(n)
     for move in moves:
         result = move[result]
     return result
@@ -12,14 +12,16 @@ def compose(moves, n):
 # move: a single move
 # inverse: computes the inverse of this move
 def inverse(move, n):
-    result = np.array(list(range(n)))
+    result = np.arange(n)
     result[move] = result
     return result
 
+# generators: an np.array of moves
+# k: the current index
 # computes Schreiers vector
 def compute_schreiers_vector(generators, n, k):
     result = [np.zeros(n, dtype = int) for _ in range(n)]
-    result[k] = np.array(list(range(n)))
+    result[k] = np.arange(n)
     v = [False for _ in range(n)]
     v[k] = True
     counter = 1
@@ -40,12 +42,16 @@ def compute_schreiers_vector(generators, n, k):
                         counter += 1
     return result
 
+# generators: an np.array of generators
+# schreiers_vector: Schreier's vector of the current stabilizer group
+# schreiers_vector_non_zero: all non-zero entries in Schreier's vector
+# k: the current index
 # computes a generating set for the next stabilizer group
-def generating_set(temp_generators, schreiers_vector, schreiers_vector_non_zero, n, k):
+def generating_set(generators, schreiers_vector, schreiers_vector_non_zero, n, k):
     result = []
     # use helper set to efficiently check membership (np.array doesn't support hashing)
     helper_set = set()
-    for a in temp_generators:
+    for a in generators:
         for u in schreiers_vector_non_zero:
             au = compose([u, a], n)
             phi = schreiers_vector[au[k]]
@@ -55,6 +61,7 @@ def generating_set(temp_generators, schreiers_vector, schreiers_vector_non_zero,
                 result.append(new_generator)
     return result
 
+# move: a single move
 # finds first index i where move[i] != i (used in Sims filter)
 def pair(move):
     i = 0
@@ -62,13 +69,14 @@ def pair(move):
         i += 1
     return i, move[i]
 
+# generators: a np.array of generators
 # restricts the number of generators to a maximum of n(n-1)/2
 def sims_filter(generators, n):
     table = np.zeros((n, n, n), dtype = int)
     k = 0
     while (k < len(generators)):
         move = generators[k]
-        if np.array_equal(move, np.array(list(range(n)))):
+        if np.array_equal(move, np.arange(n)):
             k += 1
         else:
             i, j = pair(move)
