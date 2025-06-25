3176b13a2930075438417fdb9a58db08e9191b11
shaolunzheng <shaoluncd@gmail.com>
2025-06-25 16:52:29 +0200

    added the automatic cube enumeration into schreier-sims, changed generators from lists to sets

--- functions.py
+++ functions.py
@@ -1,48 +1,65 @@
 import numpy as np
+from sympy.combinatorics import Permutation
 
-# moves: an list of moves
-# n: order of symmetric group
-# compose(): composes the elements in moves in order
-def compose(moves, n):
-    result = np.arange(n)
-    for move in moves:
-        result = move[result]
-    return result
+def enumerate(n, N):
+    turns = set()
 
-# move: a single move
-# n: order of symmetric group
-# inverse(): computes the inverse of this move
-def inverse(move, n):
-    result = np.arange(n)
-    result[move] = result
-    return result
+    if n % 2 == 0:
+        a = n**2 # number of stickers on each face
+        b = a//4 # number of 4-clusters on each face
+        c = n//2 # side length of 4-cluster
+        d = n//2 # side width of 4-cluster
+    else:
+        a = n**2-1 # number of (labeled, i.e. without middle center) stickers on each face
+        b = a//4 # number of 4-clusters on each face
+        c = (n+1)//2 # side length of 4-cluster
+        d = (n-1)//2 # side width of 4-cluster
+
+    right = [a+b, 2*a+b, 4*a, 5*a, a+2*b, 2*a+2*b, 4*a+b, 5*a+b, 0]
+    front = [3*b, 5*a+3*b, 3*a+2*b, 2*a+2*b, 0, 5*a, 3*a+3*b, 2*a+3*b, a]
+    up = [0, a, 3*a+b, 4*a+b, b, a+b, 3*a+2*b, 4*a+2*b, 2*a]
+    left = [a+3*b, 5*a+2*b, 4*a+2*b, 2*a+3*b, a, 5*a+3*b, 4*a+3*b, 2*a, 3*a]
+    back = [b, 2*a, 3*a, 5*a+b, 2*b, 2*a+b, 3*a+b, 5*a+2*b, 4*a]
+    down = [2*b, 4*a+3*b, 3*a+3*b, a+2*b, 3*b, 4*a, 3*a, a+3*b, 5*a]
+
+    faces = [right, front, up, left, back, down]
+
+    for face in faces:
+        for k in range(0, d):
+            P = Permutation(N-1)
+            for i in range(0, c):
+                P*= Permutation(face[0]+k*c+i, face[1]+k*c+i, face[2]+k*c+i, face[3]+k*c+i)
+            for j in range(0, d):
+                P*= Permutation(face[4]+j*c+k, face[5]+j*c+k, face[6]+j*c+k, face[7]+j*c+k)
+            if k == 0:
+                for l in range(0, b):
+                    P*= Permutation(face[8]+l, face[8]+l+b, face[8]+l+2*b, face[8]+l+3*b)
+            turns.add(P)
+
+    return turns
 
 # generators: a list of moves
 # n: order of symmetric group
 # k: the current index
 # compute_schreiers_vector(): computes Schreier's vector
-def compute_schreiers_vector(generators, n, k):
-    result = [np.zeros(n, dtype = int) for _ in range(n)]
-    result[k] = np.arange(n)
-    v = [False for _ in range(n)]
-    v[k] = True
+def compute_schreiers_vector(generators, N, k):
+    vector = np.full(N, None)
+    vector[k] = Permutation(N-1)
     counter = 1
     while counter > 0:
         counter = 0
-        for i in range(n):
-            if v[i]:
+        for i in range(N):
+            if vector[i] is not None:
                 for generator in generators:
-                    j = generator[i]
-                    if not v[j]:
-                        v[j] = True
-                        result[j] = compose([result[i], generator], n)
+                    j = generator(i)
+                    if vector[j] is None:
+                        vector[j] = vector[i] * generator
                         counter += 1
-                    j = inverse(generator, n)[i]
-                    if not v[j]:
-                        v[j] = True
-                        result[j] = compose([result[i], inverse(generator, n)], n)
+                    j = (generator**-1)(i)
+                    if vector[j] is None:
+                        vector[j] = vector[i] * (generator**-1)
                         counter += 1
-    return result
+    return vector
 
 # generators: a list of generators
 # schreiers_vector: Schreier's vector of the current stabilizer group
@@ -50,48 +67,39 @@ def compute_schreiers_vector(generators, n, k):
 # n: order of symmetric group
 # k: the current index
 # generating_set(): computes a generating set for the next stabilizer group
-def generating_set(generators, schreiers_vector, schreiers_vector_non_zero, n, k):
-    result = []
-    # use helper set to efficiently check membership (np.array doesn't support hashing)
-    helper_set = set()
+def generating_set(generators, schreiers_vector, schreiers_vector_non_zero, k):
+    generating_set = set()
     for a in generators:
         for u in schreiers_vector_non_zero:
-            au = compose([u, a], n)
-            phi = schreiers_vector[au[k]]
-            new_generator = compose([au, inverse(phi, n)], n)
-            if tuple(new_generator) not in helper_set:
-                helper_set.add(tuple(new_generator))
-                result.append(new_generator)
-    return result
+            au = u * a
+            phi = schreiers_vector[au(k)]
+            new_generator = au * phi**-1
+            generating_set.add(new_generator)
+    return generating_set
 
 # move: a single move
 # pair(): finds first index i where move[i] != i (used in Sims filter)
 def pair(move):
     i = 0
-    while (move[i] == i):
+    while (move(i) == i):
         i += 1
-    return i, move[i]
+    return i, move(i)
 
 # generators: a list of generators
 # n: order of symmetric group
 # sims_filter(): restricts the number of generators to a maximum of n(n-1)/2
-def sims_filter(generators, n):
-    table = np.zeros((n, n, n), dtype = int)
-    k = 0
-    while (k < len(generators)):
-        move = generators[k]
-        if np.array_equal(move, np.arange(n)):
-            k += 1
-        else:
+def sims_filter(generators, N):
+    table = np.full((N, N), None)
+    for generator in generators:
+        move = generator
+        while True:
+            if move == Permutation(N-1):
+                break
             i, j = pair(move)
-            entry = table[i,j]
-            if not np.any(entry != 0):
-                table[i,j] = move
-                k += 1
+            entry = table[i, j]
+            if entry is None:
+                table[i, j] = move
+                break
             else:
-                generators[k] = compose([entry, inverse(move, n)], n)
-
-    helper_table = np.any(table != 0, axis = 2)
-    result = [generator for generator in table[helper_table]]
-
-    return result
+                move = entry * move**-1
+    return {move for move in table.flatten() if move is not None}
